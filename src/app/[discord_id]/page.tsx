67887d0e6c8c83fa3a0a047692ed03383fbca647
Clemente Solorio <solorioeclem164@gmail.com>
2023-10-10 19:47:05 -0700

    get rid of console log

--- src/app/[discord_id]/page.tsx
+++ src/app/[discord_id]/page.tsx
@@ -1,6 +1,5 @@
 import Song from "@/app/[discord_id]/song";
 
 export default function Page({ params }: { params: { discord_id: string } }) {
-  console.log(params);
   return <Song discord_id={params.discord_id} />;
 }
