77a0c898aa20dbf4ce5df13abd4ff892eceb1759
Clemente Solorio <solorioeclem164@gmail.com>
2023-10-24 06:27:36 -0700

    rerender progress bar only

--- src/app/[discord_id]/song.tsx
+++ src/app/[discord_id]/song.tsx
@@ -4,8 +4,40 @@ import Image from "next/image";
 
 // @ts-ignore
 import { useLanyardWS } from "use-lanyard";
-import { useEffect, useState } from "react";
 import { useThrottle } from "@/lib/throttle";
+import { memo, useEffect, useState } from "react";
+
+interface ProgressProps {
+  start: number;
+  end: number;
+}
+
+const ProgressBar = memo(({ start, end }: ProgressProps) => {
+  const [, rerender] = useState({});
+
+  const total = end - start;
+  const progress = 100 - (100 * (end - new Date().getTime())) / total;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      rerender({});
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [start, end]);
+
+  return (
+    <div className="pr-6">
+      <div className="h-3 w-full rounded-full bg-white/50">
+        <div
+          className="h-3 rounded-full bg-white/50 transition-all duration-1000 ease-linear will-change-[width]"
+          style={{ width: `${progress}%` }}
+        />
+      </div>
+    </div>
+  );
+});
+ProgressBar.displayName = "ProgressBar";
 
 interface SongProps {
   discord_id: string;
@@ -16,23 +48,11 @@ type DiscordIdType = `${bigint}`;
 const Song = ({ discord_id }: SongProps) => {
   const data = useThrottle(useLanyardWS(discord_id as DiscordIdType));
 
-  const [, rerender] = useState({});
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      rerender({});
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
-
   if (!data || !data.spotify) {
     return <></>;
   }
 
-  const total = data.spotify.timestamps.end - data.spotify.timestamps.start;
-  const progress =
-    100 - (100 * (data.spotify.timestamps.end - new Date().getTime())) / total;
+  const { start, end } = data.spotify.timestamps;
 
   return (
     <div className="flex h-[200px] w-[800px] space-x-8 rounded-[18px] border-2 border-neutral-800 bg-black/60 p-3">
@@ -57,50 +77,10 @@ const Song = ({ discord_id }: SongProps) => {
             {data?.spotify?.artist}
           </p>
         </div>
-        <div className="pr-6">
-          <div className="h-3 w-full rounded-full bg-white/50">
-            <div
-              className="h-3 rounded-full bg-white/50 transition-all duration-1000 ease-linear will-change-[width]"
-              style={{ width: `${progress}%` }}
-            />
-          </div>
-        </div>
+        <ProgressBar start={start} end={end} />
       </div>
     </div>
   );
 };
-
-// original
-// export const Song = ({ data, progress }: SongProps) => {
-//   return (
-//     <div className="h-full w-full p-1">
-//       <div className="flex h-full space-x-8 rounded-2xl border-2 border-neutral-800 bg-black/60 p-4">
-//         <div className="flex-shrink-0">
-//           <img
-//             src={data.album_art_url!}
-//             alt={data.song}
-//             className="aspect-square h-full rounded-md"
-//           />
-//         </div>
-
-//         <div className="flex w-full flex-col justify-center space-y-4 overflow-hidden">
-//           <div className="space-y-2">
-//             <p className="truncate text-5xl font-bold">{data.song}</p>
-//             <p className="text- truncate text-3xl">{data.artist}</p>
-//           </div>
-
-//           <div className="pr-6">
-//             <div className="h-3 w-full rounded-full bg-white/50">
-//               <div
-//                 className="h-3 rounded-full bg-white/50 transition-all duration-1000 ease-linear will-change-[width]"
-//                 style={{ width: `${progress}%` }}
-//               ></div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
 
 export default Song;
--- src/app/song.tsx
+++ src/app/song.tsx
@@ -47,37 +47,4 @@ const Song = ({ data, progress, mouseX, mouseY }: SongProps) => {
   );
 };
 
-// original
-// export const Song = ({ data, progress }: SongProps) => {
-//   return (
-//     <div className="h-full w-full p-1">
-//       <div className="flex h-full space-x-8 rounded-2xl border-2 border-neutral-800 bg-black/60 p-4">
-//         <div className="flex-shrink-0">
-//           <img
-//             src={data.album_art_url!}
-//             alt={data.song}
-//             className="aspect-square h-full rounded-md"
-//           />
-//         </div>
-
-//         <div className="flex w-full flex-col justify-center space-y-4 overflow-hidden">
-//           <div className="space-y-2">
-//             <p className="truncate text-5xl font-bold">{data.song}</p>
-//             <p className="text- truncate text-3xl">{data.artist}</p>
-//           </div>
-
-//           <div className="pr-6">
-//             <div className="h-3 w-full rounded-full bg-white/50">
-//               <div
-//                 className="h-3 rounded-full bg-white/50 transition-all duration-1000 ease-linear will-change-[width]"
-//                 style={{ width: `${progress}%` }}
-//               ></div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
 export default Song;
